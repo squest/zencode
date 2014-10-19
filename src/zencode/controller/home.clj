b19788fa43a83919dbb513be9d30382ceba62e85
Paul Sabda <sabdaps@gmail.com>
2014-10-19 22:32:41 +0700

    tab

--- src/zencode/controller/home.clj
+++ src/zencode/controller/home.clj
@@ -1,34 +1,35 @@
 (ns zencode.controller.home
-	(:require [zencode.dbase :as dbase]
-						[com.ashafa.clutch :as cl]
-						[noir.util.crypt :as crip]))
+  (:require [zencode.dbase :as dbase]
+            [com.ashafa.clutch :as cl]
+            [noir.util.crypt :as crip]))
 
 (def cdb dbase/cdb)
 
 ;; well ini sekedar buat supaya ga di cached
 
 (defn get-user
-	[username]
-	(->> (cl/get-view cdb "user" "byUsername" {:key username})
-			 (first)
-			 (:value)))
+  [username]
+  (->> (cl/get-view cdb "user" "byUsername" {:key username})
+       (first)
+       (:value)))
 
 (defn signup
-	[kmap]
-	(let [{:keys [username password]} kmap
-				data {:username username
-							:ctype "User"
-							:password (crip/encrypt password)}]
-		(if (cl/put-document cdb data)
-			true
-			false)))
+  [kmap]
+  (let [{:keys [username password]} kmap
+        data {:username username
+              :ctype "User"
+              :password (crip/encrypt password)}]
+    (if (cl/put-document cdb data)
+      true
+      false)))
 
 (defn valid-user?
-	[kmap]
-	(let [{:keys [password]}
-				(get-user (:username kmap))]
-		(crip/compare (:password kmap)
-									password)))
+  [kmap]
+  (let [{:keys [password]}
+        (get-user (:username kmap))]
+    (crip/compare (:password kmap)
+                  password)))
+
 
 
 
